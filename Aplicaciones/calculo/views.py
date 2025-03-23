b5b5664b59013f47816fb303a2e6cc05d3c307c4
Diamante47 <137074345+Diamante47@users.noreply.github.com>
2025-03-23 16:15:07 -0500

    Subir staticfiles f

--- Aplicaciones/calculo/views.py
+++ Aplicaciones/calculo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+def inicio (request): #funcion para presentar en pantalla el codigo html del template inicio.html
+    return render(request,'inicio.html')
 
 # Create your views here.
