60f4bf35a0f723d4b2c50e932f2aef80b33a3be4
FerTipan <137077858+FerTipan@users.noreply.github.com>
2025-03-24 16:38:32 -0500

    Añadiendo plantilla y normalizando

--- Aplicaciones/calculo/views.py
+++ Aplicaciones/calculo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def tipoGeneracion(request):
+    return render(request, 'tipoGeneracion.html')
