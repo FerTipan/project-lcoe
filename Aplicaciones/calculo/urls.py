a1f26a1a88ab7d3ec7a1f9061d5b20da35851968
Diamante47 <137074345+Diamante47@users.noreply.github.com>
2025-03-24 22:41:43 -0500

    Inicio y seleccion de energia

--- Aplicaciones/calculo/urls.py
+++ Aplicaciones/calculo/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('tipo/', views.tipoGeneracion, name='tipoGeneracion'),
-    
+    path('tipoGeneracion/', views.tipoGeneracion, name='tipoGeneracion'),
+    path('hidraulica/', views.hidraulica, name='hidraulica'),
+    path('termica/', views.termica, name='termica'),
+    path('solar/', views.solar, name='solar'),  
+    path('eolica/', views.eolica, name='eolica'),
 ]
