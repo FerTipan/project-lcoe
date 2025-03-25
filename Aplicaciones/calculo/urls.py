2a3f18a2553e549ffb00e574ffed76e5e1d9e23a
FerTipan <137077858+FerTipan@users.noreply.github.com>
2025-03-24 22:44:26 -0500

    Creacion de templates

--- Aplicaciones/calculo/urls.py
+++ Aplicaciones/calculo/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('tipo/', views.tipoGeneracion, name='tipoGeneracion'),
-    
+    path('eolica/', views.eolica, name='eolica'),
+    path('solar/', views.solar, name='solar'),
+    path('hidraulica/', views.hidraulica, name='hidraulica'),
+    path('termica/', views.termica, name='termica'),
 ]
--- Aplicaciones/calculo/views.py
+++ Aplicaciones/calculo/views.py
@@ -7,3 +7,15 @@ def inicio(request):
 
 def tipoGeneracion(request):
     return render(request, 'tipoGeneracion.html')
+
+def eolica(request):
+    return render(request, 'tecnologias/eolica.html')
+
+def solar(request):
+    return render(request, 'tecnologias/solar.html')
+
+def hidraulica(request):
+    return render(request, 'tecnologias/hidraulica.html')
+
+def termica(request):
+    return render(request, 'tecnologias/termica.html')
